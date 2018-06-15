5ed1fb1e6daff8fa010fb9f16035f97fccedc3fa
Gassó Loncan, Juan Cruz <jcgassoloncan@ingenieria.uner.edu.ar>
2018-06-15 12:30:51 -0300

    cleaning

--- src/workspace/project_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/Datos/Juanc/Udacity/Localization/src/workspace/project_ws/src
-# Build directory: /mnt/Datos/Juanc/Udacity/Localization/src/workspace/project_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(udacity_bot)
